e587045efd78417d3f5a1d1488c1a23ec0d5bce3
phil-kahrl <phil_kahrl@hotmail.com>
2023-12-27 15:20:24 -0800

    Added target files.

---
+++ target/debug/deps/wasm_server-3df50942d865d51d.d
@@ -0,0 +1,5 @@
+/Users/philkahrl/workspace/wasm-server/target/debug/deps/wasm_server-3df50942d865d51d: src/main.rs
+
+/Users/philkahrl/workspace/wasm-server/target/debug/deps/wasm_server-3df50942d865d51d.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/wasm-server.d
@@ -0,0 +1,1 @@
+/Users/philkahrl/workspace/wasm-server/target/debug/wasm-server: /Users/philkahrl/workspace/wasm-server/src/main.rs
