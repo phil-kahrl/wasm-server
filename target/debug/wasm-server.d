df63c1adfaf04a5c4dc4d310209ed28eafe9613f
phil-kahrl <phil_kahrl@hotmail.com>
2023-12-28 14:40:42 -0800

    Added http server.

--- target/debug/wasm-server.d
+++
@@ -1,1 +0,0 @@
-/Users/philkahrl/workspace/wasm-server/target/debug/wasm-server: /Users/philkahrl/workspace/wasm-server/src/main.rs
--- target/wasm32-wasi/release/deps/wasm_server-f55c24d44b7134b4.d
+++
@@ -1,5 +0,0 @@
-/Users/philkahrl/workspace/wasm-server/target/wasm32-wasi/release/deps/wasm_server-f55c24d44b7134b4.wasm: src/main.rs
-
-/Users/philkahrl/workspace/wasm-server/target/wasm32-wasi/release/deps/wasm_server-f55c24d44b7134b4.d: src/main.rs
-
-src/main.rs:
