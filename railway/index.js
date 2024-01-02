66d5e979b3b42a4e341d5815cc22aa1f51d558d6
phil-kahrl <phil_kahrl@hotmail.com>
2024-01-02 14:59:57 -0800

    Added add service mutation.

--- railway/index.js
+++ railway/index.js
@@ -12,8 +12,18 @@ const schema = buildSchema(`
     name: String
     email: String
   }
-`);
 
+  type Source {
+    image: String
+  }
+
+  type ServiceCreateInput {
+    projectId: String
+    source: Source
+  }
+
+
+`);
 
 const apiToken = process.env[API_TOKEN_ENV_KEY];
 
@@ -28,8 +38,29 @@ const apiUrl = 'https://backboard.railway.app/graphql/v2';
 const whoami = () => {
     const query = `query { me { name email } }`;
     axios.post(apiUrl, { query }, { headers }).then(response => {
-        const userData = response.data
-        console.log(userData);
+        console.log(response.data);
+    }).catch(error => {
+        console.error('Error making GraphQL request:', error.message);
+    });
+}
+
+const projectId = "a1cbe294-e251-4b32-b865-4f0e7fe66925";
+const image = "hub.docker.com/r/philkahrl/wasm-server";
+
+const createService = () => {
+    const query = `mutation serviceCreate {
+        serviceCreate(
+            input: {
+                projectId: "${projectId}"
+                source: { image: "${image}" }
+            }
+        ) {
+            id
+        }
+    }`;
+    
+    axios.post(apiUrl, { query, operationName: "serviceCreate" }, { headers }).then(response => {
+        console.log(response.data);
     }).catch(error => {
         console.error('Error making GraphQL request:', error.message);
     });
